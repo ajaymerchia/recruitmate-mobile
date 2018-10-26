37dcb920d83baad1c780bdee8738fae393c3a20d
ajaymerchia <ajaymerchia@berkeley.edu>
2018-10-25 22:10:23 -0700

    favorite board on signup

--- RecruitMate/RecruitMate/ViewControllers/Onboarding/SignUpVC/SignUp-logic.swift
+++ RecruitMate/RecruitMate/ViewControllers/Onboarding/SignUpVC/SignUp-logic.swift
@@ -67,7 +67,9 @@ extension SignUpVC {
                 let firstBoardID = Utils.uuid()
                 let firstBoard = Board(title: "\(first!)'s Board", uuid: firstBoardID)
                 let boardDict: [String: String] = [firstBoardID : firstBoard.name]
-                let values = ["first": first, "last": last, "email": email, "boards": boardDict] as [String : Any]
+                
+                
+                let values = ["first": first, "last": last, "email": email, "boards": boardDict, "defaultBoardID": firstBoardID] as [String : Any]
                 
                 let boardRef = ref.child("boards").child(firstBoardID)
                 let boardValues = ["title": firstBoard.name]
