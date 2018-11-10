324d9e50f61b8ec44610b14effb2837dbd9a0132
ajaymerchia <ajaymerchia@berkeley.edu>
2018-11-10 12:34:55 -0800

    Created firebase I/O functionality

--- RecruitMate/RecruitMate/Models/Contact.swift
+++ RecruitMate/RecruitMate/Models/Contact.swift
@@ -17,13 +17,23 @@ class Contact: FirebasePushable {
         return values
     }
     
+    var id: String!
     var name: String!
     var email: String!
     var position: String!
     
     init(name: String, email: String, position: String) {
+        self.id = Utils.uuid()
         self.name = name
         self.email = email
         self.position = position
     }
+    
+    init(key: String, firebaseStruct: [String: Any?]) {
+        self.id = key
+        self.name = (firebaseStruct["name"] as! String)
+        self.email = (firebaseStruct["email"] as! String)
+        self.position = (firebaseStruct["position"] as! String)
+    }
+    
 }
